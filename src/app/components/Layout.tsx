816844e6c5d265c25c88c2b223419d1773625b0d
Zuly (권정) <zuly.kwon@goorm.io>
2025-02-27 09:40:54 +0900

    [Desgin] #3 - 배경 그라디언트 추가 및 색상 변경

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -4,13 +4,15 @@
 import React, { PropsWithChildren } from 'react';
 
 const Layout: React.FC<PropsWithChildren<{}>> = ({ children }) => (
-    <div className="container mx-auto p-4 bg-white/70 backdrop-filter backdrop-blur rounded-lg shadow-lg min-h-[calc(100vh-8rem)]">
-        <header className="py-4 mb-8">
-            <h1 className="text-4xl font-bold text-center text-gray-900">스트레스 감지 심리테스트</h1>
-        </header>
-        <main>
-            {children}
-        </main>
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-gray-900 via-purple-900 to-gray-900 bg-cover animate-gradient-x">
+        <div className="container mx-auto p-4 backdrop-filter backdrop-blur-md bg-white/10 rounded-lg min-h-[calc(100vh-8rem)]">
+            <header className="py-4 mb-8">
+                <h1 className="text-4xl font-bold text-center text-gray-100">스트레스 감지 심리테스트</h1>
+            </header>
+            <main>
+                {children}
+            </main>
+        </div>
     </div>
 );
 
--- src/app/components/Question.tsx
+++ src/app/components/Question.tsx
@@ -1,29 +1,56 @@
 // src/app/components/Question.tsx
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 interface QuestionProps {
     question: {
         text: string;
         options: string[];
+        hints: string[];
     };
     handleAnswer: (answer: string) => void;
 }
 
-const Question: React.FC<QuestionProps> = ({ question, handleAnswer }) => (
-    <div className="my-4 bg-white/70 backdrop-filter backdrop-blur rounded p-4 shadow-lg">
-        <h2 className="text-2xl mb-4 text-gray-700">{question.text}</h2>
-        <div>
-            {question.options.map((option, index) => (
-                <button
-                    key={index}
-                    onClick={() => handleAnswer(option)}
-                    className="bg-gradient-to-r from-blue-400 to-purple-600 text-white py-2 px-4 rounded-full m-1 transition-transform transform hover:scale-105"
-                >
-                    {option}
-                </button>
-            ))}
+const Question: React.FC<QuestionProps> = ({ question, handleAnswer }) => {
+    const [hint, setHint] = useState('');
+    const [showHint, setShowHint] = useState(false);
+
+    useEffect(() => {
+        let timer: NodeJS.Timeout;
+        if (showHint) {
+            timer = setTimeout(() => {
+                setShowHint(false);
+            }, 1000); 
+        }
+        return () => clearTimeout(timer);
+    }, [showHint]);
+
+    const handleOptionClick = (option: string, index: number) => {
+        setHint(question.hints[index]);
+        setShowHint(true);
+        handleAnswer(option);
+    };
+
+    return (
+        <div className="relative my-4 backdrop-blur rounded p-4 shadow-lg min-h-screen">
+            <h2 className="text-2xl mb-4 text-white">{question.text}</h2>
+            <div className="flex flex-col max-w-xs mx-auto">
+                {question.options.map((option, index) => (
+                    <button
+                        key={index}
+                        onClick={() => handleOptionClick(option, index)}
+                        className="bg-gradient-to-r to-purple-600 text-white py-2 px-4 rounded-full m-1 transition-transform transform hover:scale-105"
+                    >
+                        {option}
+                    </button>
+                ))}
+            </div>
+            {showHint && (
+                <div className="fixed bottom-4 left-1/2 transform -translate-x-1/2 p-4 bg-yellow-200 text-yellow-800 rounded-lg shadow-md transition-opacity duration-1000 ease-in-out opacity-100">
+                    {hint}
+                </div>
+            )}
         </div>
-    </div>
-);
+    );
+};
 
 export default Question;
