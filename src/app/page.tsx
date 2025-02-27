b199e6076701834bcde713e2a4004fbb59217b1f
Zuly (권정) <zuly.kwon@goorm.io>
2025-02-27 14:37:36 +0900

    [Feat] #3 - 시작 페이지 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,53 +1,6 @@
 // src/app/page.tsx
-'use client';
+import StartPage from './components/StartPage';
 
-import React, { useState } from 'react';
-import Layout from './components/Layout';
-import QuestionList from './components/QuestionList';
-import Result from './components/Result';
-import LoadingView from './components/LoadingView';
-
-const HomePage: React.FC = () => {
-  const [result, setResult] = useState<string | null>(null);
-  const [isLoading, setIsLoading] = useState(false);
-
-  const handleComplete = async (allQuestionsAndAnswers: { question: string; answer: string }[]) => {
-    setIsLoading(true); 
-    
-    try {
-      const response = await fetch('/api/analyze-stress', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ allQuestionsAndAnswers }),
-      });
-
-      if (!response.ok) {
-        console.error('API 응답이 실패했습니다.');
-        return;
-      }
-
-      const data = await response.json();
-      setResult(data.message);
-    } catch (error) {
-      console.error('API 요청 중 오류가 발생했습니다:', error);
-    } finally {
-      setIsLoading(false); 
-    }
-  };
-
-  return (
-    <Layout>
-      {isLoading ? (
-        <LoadingView />
-      ) : result ? (
-        <Result message={result} />
-      ) : (
-        <QuestionList onComplete={handleComplete} />
-      )}
-    </Layout>
-  );
-};
-
-export default HomePage;
+export default function Home() {
+  return <StartPage />;
+}
